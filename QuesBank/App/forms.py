0a07264d76568a474cdbbdb0c340a4ac4386357b
Divyani <rdivyani00@gmail.com>
2020-08-10 20:35:05 +0530

    msg

--- QuesBank/App/forms.py
+++
@@ -1,7 +0,0 @@
-from App.models import Data
-from django import forms
-
-class DataForm(forms.ModelForm):
-    class Meta:
-        model=Data
-        fields='__all__'
--- QuesBank/App/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Data(models.Model):
-    Questions=models.CharField(max_length=250)
-    Teacher Name=models.CharField(max_length=30)
-    Question Bank=models.CharField(max_length=4000)
